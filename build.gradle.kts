2ab0430f1d6b68377e017e5cc8937f162e182921
Jiangc <jiangc@vistring.com>
2023-06-23 18:11:06 +0800

    chore: update action

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.foodiestudio"
-version = "2023.01.00-rc.10"
+version = "2023.01.00-rc.11"
 
 catalog {
     // declare the aliases, bundles and versions in this block
